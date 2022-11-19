c2983c7fc722c0e7f7ac9c7bb202f6b797fe5739
kli2013 <ayubqb@163.com>
2022-11-19 10:46:45 +0800

    Update mpv添加添加三分屏右键菜单.bat

--- mpv添加添加三分屏右键菜单.bat
+++ mpv添加添加三分屏右键菜单.bat
@@ -3,6 +3,8 @@ Title Reg Converter v1.2 & Color 1A
 cd %systemroot%\system32
 call :IsAdmin
 
+rem 放到mpv.exe同目录运行
+
 Reg.exe add "HKLM\SOFTWARE\Classes\io.mpv.3gp2\shell\open1" /ve /t REG_SZ /d "使用 MPV 水平分三屏 播放" /f
 Reg.exe add "HKLM\SOFTWARE\Classes\io.mpv.3gp2\shell\open1\command" /ve /t REG_SZ /d "\"%CD%\mpv.exe\" \"%%L\" --external-file=\"%%L\" --external-file=\"%%L\" --lavfi-complex=\"[vid1] [vid2] hstack [t1] ; [t1] [vid3] hstack [vo]\"" /f
 Reg.exe add "HKLM\SOFTWARE\Classes\io.mpv.3gpp\shell\open1" /ve /t REG_SZ /d "使用 MPV 水平分三屏 播放" /f
